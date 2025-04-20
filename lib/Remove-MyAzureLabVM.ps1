22261b7a3ea1ce02a9040cfc7d64ec33e9f7dbec
Andreas Jordan <anj@ordix.de>
2025-04-20 13:56:13 +0200

    new feature

--- lib/Remove-MyAzureLabVM.ps1
+++ lib/Remove-MyAzureLabVM.ps1
@@ -1,17 +1,23 @@
 function Remove-MyAzureLabVM {
     [CmdletBinding()]
     Param(
-        [string]$ComputerName,
+        [string[]]$ComputerName,
+        [switch]$All,
         [switch]$EnableException
     )
 
     process {
         try {
-            Write-PSFMessage -Level Verbose -Message "Removing virtual maschine $ComputerName"
-            $null = Remove-AzVM -Name "$($ComputerName)_VM" -ResourceGroupName $resourceGroupName -Force
-            $null = Remove-AzDisk -DiskName "$($ComputerName)_Disk1.vhd" -ResourceGroupName $resourceGroupName -Force
-            $null = Remove-AzNetworkInterface -Name "$($ComputerName)_Interface" -ResourceGroupName $resourceGroupName -Force
-            $null = Remove-AzPublicIpAddress -Name "$($ComputerName)_PublicIP" -ResourceGroupName $resourceGroupName -Force
+            if ($All) {
+                $ComputerName = Get-AzVM -ResourceGroupName $resourceGroupName | ForEach-Object -Process { $_.Name -replace '_VM$', '' }
+            }
+            foreach ($name in $ComputerName) {
+                Write-PSFMessage -Level Verbose -Message "Removing virtual maschine $name"
+                $null = Remove-AzVM -Name "$($name)_VM" -ResourceGroupName $resourceGroupName -Force
+                $null = Remove-AzDisk -DiskName "$($name)_Disk1.vhd" -ResourceGroupName $resourceGroupName -Force
+                $null = Remove-AzNetworkInterface -Name "$($name)_Interface" -ResourceGroupName $resourceGroupName -Force
+                $null = Remove-AzPublicIpAddress -Name "$($name)_PublicIP" -ResourceGroupName $resourceGroupName -Force
+            }
         } catch {
             Stop-PSFFunction -Message 'Failed' -ErrorRecord $_ -EnableException $EnableException
         }
