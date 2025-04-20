c1ba88c89998ba0f77d0b4787a7290453ddb3cc3
Andreas Jordan <anj@ordix.de>
2025-04-20 13:55:40 +0200

    Small updates

--- init_JustForFun.ps1
+++ init_JustForFun.ps1
@@ -3,10 +3,9 @@
 . .\MyAzureLab.ps1
 
 # Name of resource group and location
-# Will be used by MyAzureLab commands
+# Will be used by MyAzureLab commands (so these are "global" variables)
 $resourceGroupName = 'JustForFun'
-$location = 'Germany West Central'  # nearer
-#$location = 'North Europe'          # cheaper
+$location = 'North Europe'
 
 # Name and password of the initial account
 $initUser     = 'initialAdmin'     # Will be used when creating the virtual maschines
@@ -19,8 +18,8 @@ Show-MyAzureLabResourceGroupInfo
 # Don't do anything else
 break
 
-# Maybe needed more than once?
-Update-AzConfig -DisplayBreakingChangeWarning $false
+# To suppress the warnings about breaking changes:
+# Update-AzConfig -DisplayBreakingChangeWarning $false
 
 
 # Daily tasks if the lab is fully set up:
@@ -31,29 +30,55 @@ Start-MyAzureLabResourceGroup
 Stop-MyAzureLabResourceGroup
 
 
-Start-MyAzureLabRDP -ComputerName SERVER -Credential $initCredential
+Start-MyAzureLabRDP -ComputerName WIN11 -Credential $initCredential
 
 $psSession = New-MyAzureLabSession -ComputerName SERVER -Credential $initCredential
 $psSession | Remove-PSSession
 
+$session = New-MyAzureLabSSHSession -ComputerName ALMA9 -Credential $initCredential
+$null = Invoke-SSHCommand -SSHSession $session -Command 'pwd' -ShowStandardOutputStream 
 
 
 # Tasks to create and remove virtual maschines:
 ###############################################
 
 
-New-MyAzureLabVM -ComputerName SERVER -SourceImage WindowsServer2025WSL -VMSize Standard_E4s_v5 -Credential $initCredential
+# Standard_B2s:    2 vCPU /  4 GB RAM / 0,0496 Euro/h / no TrustedLaunch
+# Standard_B2s_v2  2 vCPU /  8 GB RAM / 0,0926 Euro/h / TrustedLaunch
+# Standard_B4s_v2: 4 vCPU / 16 GB RAM / 0,1862 Euro/h / TrustedLaunch
+# Standard_E4s_v6: 4 vCPU / 32 GB RAM / 0,4445 Euro/h / TrustedLaunch
 
-New-MyAzureLabVM -ComputerName SERVER -SourceImage WindowsServer2025WSL -VMSize Standard_E4s_v5 -Credential $initCredential -TrustedLaunch
+New-MyAzureLabVM -ComputerName WIN11 -SourceImage Windows11 -VMSize Standard_B2s_v2 -Credential $initCredential -TrustedLaunch
+
+New-MyAzureLabVM -ComputerName SRV2022 -SourceImage WindowsServer2022 -VMSize Standard_B2s_v2 -Credential $initCredential -TrustedLaunch
+New-MyAzureLabVM -ComputerName SRV2025 -SourceImage WindowsServer2025 -VMSize Standard_B2s_v2 -Credential $initCredential -TrustedLaunch
+
+New-MyAzureLabVM -ComputerName SQL2019 -SourceImage SQLServer2019 -VMSize Standard_B4s_v2 -Credential $initCredential -TrustedLaunch
+New-MyAzureLabVM -ComputerName SQL2022 -SourceImage SQLServer2022 -VMSize Standard_B4s_v2 -Credential $initCredential -TrustedLaunch
+
+New-MyAzureLabVM -ComputerName UBUNTU22 -SourceImage Ubuntu22 -VMSize Standard_B2s -Credential $initCredential
+New-MyAzureLabVM -ComputerName UBUNTU24 -SourceImage Ubuntu24 -VMSize Standard_B2s -Credential $initCredential
+
+New-MyAzureLabVM -ComputerName ALMA8 -SourceImage AlmaLinux8 -VMSize Standard_B2s -Credential $initCredential
+New-MyAzureLabVM -ComputerName ALMA9 -SourceImage AlmaLinux9 -VMSize Standard_B2s -Credential $initCredential
 
 
-# To setup WSL2:
+Start-MyAzureLabRDP -ComputerName SRV2025 -Credential $initCredential
+# To setup WSL2 (not supported on WIN11, tested on SRV2025):
 # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 # wsl --install
 # wsl --install
 
 
 
+
+
+
+Remove-MyAzureLabVM -ComputerName WIN11 
+Remove-MyAzureLabVM -ComputerName SRV2022 
+Remove-MyAzureLabVM -ComputerName SRV2025
+
+Remove-MyAzureLabVM -All -Verbose
 
 
 
