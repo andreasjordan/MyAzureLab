202b185481c155da55bc43dc7f2ce4d26c294984
Andreas Jordan <anj@ordix.de>
2025-05-28 11:34:46 +0200

    Added retry loop

--- lib/Remove-MyAzureLabVM.ps1
+++ lib/Remove-MyAzureLabVM.ps1
@@ -7,19 +7,84 @@ function Remove-MyAzureLabVM {
     )
 
     process {
-        try {
-            if ($All) {
+        if ($All) {
+            try {
                 $ComputerName = Get-AzVM -ResourceGroupName $resourceGroupName | ForEach-Object -Process { $_.Name -replace '_VM$', '' }
+            } catch {
+                Stop-PSFFunction -Message "Get-AzVM failed: $_" -ErrorRecord $_ -EnableException $EnableException
+                return
             }
-            foreach ($name in $ComputerName) {
-                Write-PSFMessage -Level Verbose -Message "Removing virtual maschine $name"
-                $null = Remove-AzVM -Name "$($name)_VM" -ResourceGroupName $resourceGroupName -Force
-                $null = Remove-AzDisk -DiskName "$($name)_Disk1.vhd" -ResourceGroupName $resourceGroupName -Force
-                $null = Remove-AzNetworkInterface -Name "$($name)_Interface" -ResourceGroupName $resourceGroupName -Force
-                $null = Remove-AzPublicIpAddress -Name "$($name)_PublicIP" -ResourceGroupName $resourceGroupName -Force
+        }
+        foreach ($name in $ComputerName) {
+            Write-PSFMessage -Level Verbose -Message "Removing virtual maschine $name"
+            
+            $retry = 0
+            while ($true) {
+                try {
+                    Write-PSFMessage -Level Verbose -Message "Removing $($name)_VM"
+                    $null = Get-AzVM -ResourceGroupName $resourceGroupName | Where-Object Name -eq "$($name)_VM" | Remove-AzVM -Force
+                    break
+                } catch {
+                    Write-PSFMessage -Level Warning -Message "Remove-AzVM failed: $_" -ErrorRecord $_
+                    Start-Sleep -Seconds 10
+                    $retry++
+                    if ($retry -eq 3) {
+                        Stop-PSFFunction -Message "Remove-AzVM failed: $_" -ErrorRecord $_ -EnableException $EnableException
+                        return
+                    }
+                }
             }
-        } catch {
-            Stop-PSFFunction -Message 'Failed' -ErrorRecord $_ -EnableException $EnableException
+
+            $retry = 0
+            while ($true) {
+                try {
+                    Write-PSFMessage -Level Verbose -Message "Removing $($name)_Disk1.vhd"
+                    $null = Get-AzDisk -ResourceGroupName $resourceGroupName | Where-Object Name -eq "$($name)_Disk1.vhd" | Remove-AzDisk -Force
+                    break
+                } catch {
+                    Write-PSFMessage -Level Warning -Message "Remove-AzDisk failed: $_" -ErrorRecord $_
+                    Start-Sleep -Seconds 10
+                    $retry++
+                    if ($retry -eq 3) {
+                        Stop-PSFFunction -Message "Remove-AzDisk failed: $_" -ErrorRecord $_ -EnableException $EnableException
+                        return
+                    }
+                }
+            }
+
+            $retry = 0
+            while ($true) {
+                try {
+                    Write-PSFMessage -Level Verbose -Message "Removing $($name)_Interface"
+                    $null = Get-AzNetworkInterface -ResourceGroupName $resourceGroupName | Where-Object Name -eq "$($name)_Interface" | Remove-AzNetworkInterface -Force
+                    break
+                } catch {
+                    Write-PSFMessage -Level Warning -Message "Remove-AzNetworkInterface failed: $_" -ErrorRecord $_
+                    Start-Sleep -Seconds 10
+                    $retry++
+                    if ($retry -eq 3) {
+                        Stop-PSFFunction -Message "Remove-AzNetworkInterface failed: $_" -ErrorRecord $_ -EnableException $EnableException
+                        return
+                    }
+                }
+            }
+
+            $retry = 0
+            while ($true) {
+                try {
+                    Write-PSFMessage -Level Verbose -Message "Removing $($name)_PublicIP"
+                    $null = Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName | Where-Object Name -eq "$($name)_PublicIP" | Remove-AzPublicIpAddress -Force
+                    break
+                } catch {
+                    Write-PSFMessage -Level Warning -Message "Remove-AzPublicIpAddress failed: $_" -ErrorRecord $_
+                    Start-Sleep -Seconds 10
+                    $retry++
+                    if ($retry -eq 3) {
+                        Stop-PSFFunction -Message "Remove-AzPublicIpAddress failed: $_" -ErrorRecord $_ -EnableException $EnableException
+                        return
+                    }
+                }
+            }
         }
     }
 }
